e0f01f25a834a7c6b2adde11caccb88f8038b18a
Anton <anton.gusevv.01@gmail.com>
2021-05-08 23:08:53 +0300

    небольшой фикс заказа перемещения

--- db_course_work/db_course_work/Moving.cs
+++ db_course_work/db_course_work/Moving.cs
@@ -47,23 +47,31 @@ namespace db_course_work
                     }
                     reader.Close();
 
-                    numericUpDownAmount.Maximum = amountFrom;
+                    if (amountFrom >= numericUpDownAmount.Value)
+                    {
 
-                    command = new MySqlCommand("UPDATE contains SET Cont_amount = @newAm WHERE Mat_ID = @mat AND St_ID = @stor", db.GetConnection());
-                    command.Parameters.Add("@mat", MySqlDbType.Int32).Value = numericUpDownMaterialID.Value;
-                    command.Parameters.Add("@stor", MySqlDbType.Int32).Value = numericUpDownStorageIDFrom.Value;
-                    command.Parameters.Add("@newAm", MySqlDbType.Int32).Value = amountFrom - numericUpDownAmount.Value;
-                    command.Connection = db.GetConnection();
-                    command.ExecuteNonQuery();
+                        command = new MySqlCommand("UPDATE contains SET Cont_amount = @newAm WHERE Mat_ID = @mat AND St_ID = @stor", db.GetConnection());
+                        command.Parameters.Add("@mat", MySqlDbType.Int32).Value = numericUpDownMaterialID.Value;
+                        command.Parameters.Add("@stor", MySqlDbType.Int32).Value = numericUpDownStorageIDFrom.Value;
+                        command.Parameters.Add("@newAm", MySqlDbType.Int32).Value = amountFrom - numericUpDownAmount.Value;
+                        command.Connection = db.GetConnection();
+                        command.ExecuteNonQuery();
 
-                    command = new MySqlCommand("UPDATE contains SET Cont_amount = @newAm WHERE Mat_ID = @mat AND St_ID = @stor", db.GetConnection());
-                    command.Parameters.Add("@mat", MySqlDbType.Int32).Value = numericUpDownMaterialID.Value;
-                    command.Parameters.Add("@stor", MySqlDbType.Int32).Value = numericUpDownStorageIDTo.Value;
-                    command.Parameters.Add("@newAm", MySqlDbType.Int32).Value = amountTo + numericUpDownAmount.Value;
-                    command.Connection = db.GetConnection();
-                    command.ExecuteNonQuery();
-                    db.CloseConnection();
-                    Close();
+                        command = new MySqlCommand("UPDATE contains SET Cont_amount = @newAm WHERE Mat_ID = @mat AND St_ID = @stor", db.GetConnection());
+                        command.Parameters.Add("@mat", MySqlDbType.Int32).Value = numericUpDownMaterialID.Value;
+                        command.Parameters.Add("@stor", MySqlDbType.Int32).Value = numericUpDownStorageIDTo.Value;
+                        command.Parameters.Add("@newAm", MySqlDbType.Int32).Value = amountTo + numericUpDownAmount.Value;
+                        command.Connection = db.GetConnection();
+                        command.ExecuteNonQuery();
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Вы не можете переместить бо́льшее количество материала, чем имеется на складе отправки", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    
                 }
                 else
                 {
